c73032113824c0f0d4efee1576a86db14b7241c1
Bartek <bartcdx.minecraft@gmail.com>
2026-03-13 23:24:12 +0100

    added IsLiving boolean

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,7 +5,9 @@ public class Health : MonoBehaviour
     public int maxHealth = 100;
     public int health = 100;
     private int currentHealth;
-
+    
+    [SerializeField]
+    private bool IsLiving = true;
     void Start()
     {
         currentHealth = health;
@@ -16,7 +18,7 @@ public class Health : MonoBehaviour
         if(health <= 0)
         {
             health = 0; //funkcja na umieranie czy cos ewentualnie animacja lub ragdoll, ale to juz pozniej
+            IsLiving = false;
         }
-
     }
 }
