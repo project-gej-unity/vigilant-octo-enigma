afb0ae6141f6a034d8148e92dddebc1f35e04dfd
OskarMulawa <oskarmulawa.2009@gmail.com>
2026-03-14 15:54:44 +0100

    Zrobienie funkcji wykrwawiania sie

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,6 +5,7 @@ public class Health : MonoBehaviour
     public int maxHealth = 100;
     public int health = 100;
     private int currentHealth;
+    public int bleedingDamage;
     
     [SerializeField]
     private bool IsLiving = true;
@@ -12,13 +13,29 @@ public class Health : MonoBehaviour
     {
         currentHealth = health;
     }
+
+    public void startBleeding() 
+    {
+        InvokeRepeating("Bleeding", 1f, 1f); //powtarzanie krwawienia co sekunde
+    }
+
+    public void stopBleeding() 
+    {
+        CancelInvoke("Bleeding");
+    }
     public void TakeDamage(int damage)
     {
-        health = currentHealth - damage;
-        if(health <= 0)
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
         {
-            health = 0; //funkcja na umieranie czy cos ewentualnie animacja lub ragdoll, ale to juz pozniej
+            currentHealth = 0; //funkcja na umieranie czy cos ewentualnie animacja lub ragdoll, ale to juz pozniej
             IsLiving = false;
         }
+    }
+
+    void Bleeding() //funkcja na krwawienie 
+    {
+        TakeDamage(bleedingDamage);
     }
 }
